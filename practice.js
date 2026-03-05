91f589343826276b5c54c1c641f8ec51e0fe6c52
AlexandrNachmanovich <a.nachmanovich@gmail.com>
2026-03-05 06:58:26 +0200

    05.03.2026

---
+++ practice.js
@@ -0,0 +1,36 @@
+// ==============================================================================
+// Оператор разделения объекта на свойства
+/**
+const buttonInfo = {
+  text: "Buy",
+};
+
+const buttonStyle = {
+  color: "yellow",
+  width: 200,
+  height: 300,
+};
+
+const button = {
+  ...buttonInfo,
+  ...buttonStyle,
+};
+
+console.table(button);
+*/
+
+// ==============================================================================
+// Конкатенация с помощью шаблонных строк
+/**
+const hello = "Hello";
+const world = "World";
+
+const greetings = `${hello} ${world}`;
+console.log(greetings);
+
+const myName = "Alexander";
+const myCity = "Be'er Sheva";
+
+console.log(`My name is ${myName} and I'm living in ${myCity}`);
+*/
+// ==============================================================================
